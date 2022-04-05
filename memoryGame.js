289f2030923588da2e80adfd8a1e9fd70d5b8a6d
chagai <chagaiR@gmail.com>
2022-04-05 18:43:33 +0300

    add element of card

--- memoryGame.js
+++ memoryGame.js
@@ -1,5 +1,8 @@
-const cards= ['A','B','C','A','B','C'];
-const element =  document.createElement("div");
-element.innerHTML = "chagai";
+const cards= ['CHAGAI','CHAVA','DAGAN','ORI TZION','RAZ ELISHA','CHAGAI','CHAVA','DAGAN','ORI TZION','RAZ ELISHA'];
 const board =document.getElementById("board");
+
+for (i in cards){
+const element =  document.createElement("div");
+element.innerHTML = cards[i];
 board.appendChild(element);
+}
