c7cd63e949e5e5c47869df92728cf6bbd38ec618
chagai <chagaiR@gmail.com>
2022-04-05 18:20:52 +0300

    add elements

---
+++ memoryGame.js
@@ -0,0 +1,5 @@
+const cards= ['A','B','C','A','B','C'];
+const element =  document.createElement("div");
+element.innerHTML = "chagai";
+const board =document.getElementById("board");
+board.appendChild(element);
